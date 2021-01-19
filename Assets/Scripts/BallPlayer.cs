2c250357b2e1a54bf94ef5098ceee1a4428b7fd2
HabtomTsige20 <habtom.tsigehannes.b31858@mcast.edu.mt>
2021-01-19 17:19:45 +0100

    Starting codding
    Created the plateform and coding

---
+++ Assets/Scripts/BallPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballPlayer : MonoBehaviour
+{ 
+   public Rigidbody2D rb;
+   public Rigidbody2D anchor;
+   public float ReleaseTime = 5f;
+   public float maxDragDistance = 2f;
+
+   private bool isPressed = false;
+   private Vector2 mousePos;
+
+    void Update()
+   {
+       if (isPressed)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if(Vector3.Distance(mousePos, anchor.position) > maxDragDistance)
+            rb.position = (anchor.position - mousePos).normalized * maxDragDistance;
+        }
+        //
+        else
+        {
+            rb.position = mousePos;
+        }    
+    }
+   void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+    void OnMouseUp()
+    {
+         isPressed =false; 
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(ReleaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+    }
+    
+ 
+
+  
+    
+}
